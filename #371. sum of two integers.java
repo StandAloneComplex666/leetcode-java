4ae5c09acc1d1ae0e173cdf71d436e9ded5e95c4
Yancheng Chen <yc2739@nyu.edu>
2017-07-19 00:26:07 +0800

    Add files via upload

---
+++ #371. sum of two integers.java
@@ -0,0 +1,11 @@
+#371. sum of two integers
+public class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int c = a ^ b;
+            b = (a & b) << 1;
+            a = c;
+        }
+        return a;
+    }
+}
