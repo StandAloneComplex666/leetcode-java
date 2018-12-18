2f10d9b4d8e0c5ce420adb83057211d14de92e75
taqikema666 <taqikema666@gmail.com>
2018-12-19 00:06:44 +0800

    Update # 216. Combination Sum III.java

--- # 216. Combination Sum III.java
+++ # 216. Combination Sum III.java
@@ -2,26 +2,22 @@ class Solution {
     List<List<Integer>> result;
     public List<List<Integer>> combinationSum3(int k, int n) {
         result = new ArrayList<List<Integer>>();
-        int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        helper(numbers, k, n, 0, new ArrayList<>(), 0);
+        int[] numbers = new int[]{1,2,3,4,5,6,7,8,9};
+        helper(numbers, k, n, 0,new ArrayList<>(), 0);
         return result;
     }
-    
-    private void helper(int[] numbers, int k, int target, int index, List<Integer> list, int counter) {
-        if(counter > k) {
-           return;
+    private void helper(int[] numbers, int k, int target, int index, List<Integer> list, int counter){
+        if (counter > k) return;
+        if (target == 0){
+            if (counter == k) result.add(new ArrayList<>(list));
+            return;    
         }
-        if(target == 0) {
-            if(counter == k) result.add(new ArrayList<>(list));
-            return;
-        }
-        for(int i=index; i<numbers.length; i++) {
-            if(target < numbers[i]) {
-                break;
-            }
+        for (int i = index; i < numbers.length;i++ ){
+            if (target < numbers[i]) break;
             list.add(numbers[i]);
-            helper(numbers, k, target-numbers[i], i+1, list, counter+1);
-            list.remove(list.size()-1);
+            helper(numbers, k, target - numbers[i], i+1, list, counter+1);
+            //here if I choose index + 1 instead of i + 1, I will get the answer of duplicate numbers like(1,3,3),(2,2,3)
+            list.remove(list.size() - 1);
         }
     }
 }
